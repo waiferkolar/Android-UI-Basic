98600e6dd4d303a9887b9f2caefbd997939733f6
waiferkolar <72coder@gmail.com>
2020-03-08 17:57:34 +0630

    More modi

--- app/src/main/java/aa/bb/cc/dd/MainActivity.kt
+++ app/src/main/java/aa/bb/cc/dd/MainActivity.kt
@@ -3,19 +3,29 @@ package aa.bb.cc.dd
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import org.jetbrains.anko.toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        supportActionBar?.hide()
         supportActionBar?.title = "Superman"
+
+        button2.setOnClickListener {
+            val email = editText.text.toString()
+            val pass = editText2.text.toString()
+            textView.text = String.format("Email is %s and Password is %s", email, pass);
+
+            if (email.contentEquals("mgmg@gmail.com") && pass.contentEquals("123123")) {
+                imageView.setImageResource(R.drawable.happy)
+            } else {
+                imageView.setImageResource(R.drawable.angry)
+            }
+            imageView.visibility = View.VISIBLE
+
+        }
+
     }
 
-    fun showT(v:View) {
-        toast("Hey Now")
-    }
 }
